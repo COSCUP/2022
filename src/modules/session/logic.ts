5034a642b6fcc8cc8858606a9a57c703623a9e5c
yoyo930021 <yoyo930021@gmail.com>
2023-04-27 13:57:44 +0800

    Fix session records

--- src/modules/session/logic.ts
+++ src/modules/session/logic.ts
@@ -264,7 +264,7 @@ export function generateSessionPopupContentHtml (session: Session, community: { 
         <div class="record-wrapper">
           <iframe
             class="record"
-            src="https://www.youtube-nocookie.com/embed/${session.record.split('?v=').pop()}"
+            src="https://www.youtube-nocookie.com/embed/${session.record.replace('https://youtu.be/', '')}"
             frameborder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
